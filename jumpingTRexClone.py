9e7a869187bbac53795e0794a8bb95548338258d
Adrien Basyn <adrien.basyn@outlook.com>
2023-02-16 13:53:55 +0100

    Created infinite background

--- jumpingTRexClone.py
+++ jumpingTRexClone.py
@@ -1,11 +1,34 @@
 import pygame, sys
 
 #Global variables
-screenHeight = 1080
-screenWidth = 1920
+screenWidth = 1280
+screenHeight = 720
+gameVelocity = 1
 
 pygame.init()
-screen = pygame.display.set_mode((screenWidth, screenHeight), pygame.FULLSCREEN)
+screen = pygame.display.set_mode((screenWidth, screenHeight))
+clock = pygame.time.Clock()
+
+class Player:
+    pass
+
+class Enemy:
+    pass
+
+bgPosX = 0
+bgVelocity = 2 #must be an even number
+def drawBackground():
+    global bgPosX
+    print(str(bgPosX))
+    print(str(-screenWidth))
+    print('-------')
+    pygame.draw.rect(screen, (0, 0, 255), (bgPosX, 0, screenWidth, screenHeight))
+    pygame.draw.rect(screen, (255, 0, 0), (screenWidth + bgPosX, 0, screenWidth, screenHeight))
+
+    if bgPosX == -screenWidth:
+        bgPosX = 0
+    
+    bgPosX -= bgVelocity
 
 while True:
     for event in pygame.event.get():
@@ -13,4 +36,8 @@ while True:
             pygame.quit()
             sys.exit()
     
+    screen.fill((0, 0, 0))
+    drawBackground()
     pygame.display.update()
+    clock.tick(60)
+    
