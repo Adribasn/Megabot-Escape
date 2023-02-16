2a6ef83014bf7d05da869f97df5e64b77e8500cc
Adrien Basyn <adrien.basyn@outlook.com>
2023-02-16 17:53:51 +0100

    Basic implementation of random enemy spawner

--- jumpingTRexClone.py
+++ jumpingTRexClone.py
@@ -1,13 +1,17 @@
 import pygame, sys
+import random
 
 #Global variables
 screenWidth = 1280
 screenHeight = 720
-gameVelocity = 1
 
 collisionTolerance = 10
 
 groundRectHeight = 250
+
+enemyVelocity = 5
+enemyWidth = 32
+
 pygame.init()
 screen = pygame.display.set_mode((screenWidth, screenHeight))
 clock = pygame.time.Clock()
@@ -15,13 +19,13 @@ clock = pygame.time.Clock()
 class Player:
     def __init__(self, x, y):
         self.x = x
-        self.height = 32
         self.width = 32
+        self.height = 64
         self.y = y - self.height
         self.jumping = False
-        self.jumpHeight = 20
-        self.yVelocity = self.jumpHeight
-        self.gravity = 1
+        self.originalYvelocity = 15
+        self.yVelocity = self.originalYvelocity 
+        self.gravity = .75
     
     def draw(self):
         pygame.draw.rect(screen, (0, 0, 255), (self.x, self.y, self.width, self.height))
@@ -34,12 +38,20 @@ class Player:
             self.y -= self.yVelocity
             self.yVelocity -= self.gravity
 
-            if self.yVelocity < -self.jumpHeight:
+            
+            if self.yVelocity < -self.originalYvelocity:
                 self.jumping = False
-                self.yVelocity = self.jumpHeight
+                self.yVelocity = self.originalYvelocity
 
 class Enemy:
-    pass
+    def __init__(self):
+        self.width = enemyWidth
+        self.height = 64
+        self.x = screenWidth
+        self.y = screenHeight - groundRectHeight - self.height
+    
+    def draw(self):
+        pygame.draw.rect(screen, (255, 0, 0), (self.x, self.y, self.width, self.height))
 
 def checkCollision(rect1, rect2):
     if rect1.colliderect(rect2):
@@ -48,7 +60,14 @@ def checkCollision(rect1, rect2):
         if abs(rect1.bottom) - abs(rect2.top) < collisionTolerance:
             return True
 
-player = Player(50, screenHeight - groundRectHeight)
+player = Player(100, screenHeight - groundRectHeight)
+#enemy = Enemy()
+
+enemyDistance = random.randint(10, 50)
+enemyDistanceCounter = 0
+
+enemyList = []
+
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT or (event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE):
@@ -62,6 +81,18 @@ while True:
     
     player.jump()
     player.draw()
+
+    enemyDistanceCounter += 1
+    
+    if enemyDistanceCounter == enemyDistance:
+        enemyList.append(Enemy())
+        enemyDistanceCounter = 0
+        enemyDistance = random.randint(10, 50)
+    
+    for enemy in enemyList:
+        enemy.draw()
+        enemy.x -= 5
+    
     pygame.display.update()
     clock.tick(60)
     
