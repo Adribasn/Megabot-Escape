dc50039956d6f4f5a66c369556f1c5f5fd811201
Adrien Basyn <adrien.basyn@outlook.com>
2023-02-16 14:39:21 +0100

    Figured it was better to implment infinite background later so removed it for now

--- jumpingTRexClone.py
+++ jumpingTRexClone.py
@@ -15,21 +15,6 @@ class Player:
 class Enemy:
     pass
 
-bgPosX = 0
-bgVelocity = 2 #must be an even number
-def drawBackground():
-    global bgPosX
-    print(str(bgPosX))
-    print(str(-screenWidth))
-    print('-------')
-    pygame.draw.rect(screen, (0, 0, 255), (bgPosX, 0, screenWidth, screenHeight))
-    pygame.draw.rect(screen, (255, 0, 0), (screenWidth + bgPosX, 0, screenWidth, screenHeight))
-
-    if bgPosX == -screenWidth:
-        bgPosX = 0
-    
-    bgPosX -= bgVelocity
-
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT or (event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE):
