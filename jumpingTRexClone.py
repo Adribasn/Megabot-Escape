946cfd042e25c073c91afe9a8ccf98c1bdc72677
Adrien Basyn <adrien.basyn@outlook.com>
2023-02-20 18:20:08 +0100

    Infinite floor/foreground

--- jumpingTRexClone.py
+++ jumpingTRexClone.py
@@ -12,7 +12,7 @@ gameHasRun = False
 
 groundRectHeight = 250
 
-enemyVelocity = 7
+gameVelocity = 7
 
 pygame.init()
 screen = pygame.display.set_mode((screenWidth, screenHeight), flags=pygame.SCALED, vsync=1)
@@ -22,6 +22,31 @@ background = pygame.image.load('assets/bg.png')
 background = pygame.transform.scale(background, (269, 470))
 backgroundMultiples = math.ceil(screenWidth / background.get_width()) 
 backgroundX = screenWidth
+
+foreground0 = pygame.image.load('assets/tilemap/tile001.png')
+foreground1 = pygame.image.load('assets/tilemap/tile003.png')
+foreground2 = pygame.image.load('assets/tilemap/tile005.png')
+foreground3 = pygame.image.load('assets/tilemap/tile008.png')
+foreground4 = pygame.image.load('assets/tilemap/tile006.png')
+foreground5 = pygame.image.load('assets/tilemap/tile019.png')
+foreground5 = pygame.transform.scale(foreground5, (64, 64))
+foreground6 = pygame.image.load('assets/tilemap/tile021.png')
+foreground7 = pygame.image.load('assets/tilemap/tile023.png')
+foreground8 = pygame.image.load('assets/tilemap/tile024.png')
+foreground9 = pygame.image.load('assets/tilemap/tile026.png')
+foreground10 = pygame.image.load('assets/tilemap/tile028.png')
+foreground10 = pygame.transform.scale(foreground10, (64, 64))
+foreground11 = pygame.image.load('assets/tilemap/tile010.png')
+foreground12 = pygame.image.load('assets/tilemap/tile036.png')
+foreground13 = pygame.image.load('assets/tilemap/tile037.png')
+foreground14 = pygame.image.load('assets/tilemap/tile039.png')
+foreground15 = pygame.image.load('assets/tilemap/tile040.png')
+foreground16 = pygame.image.load('assets/tilemap/tile041.png')
+
+foregroundTilemap = [[5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5],
+                  [10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10],
+                  [10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10],
+                  [10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10]]
 
 playerIdle = pygame.image.load('assets\player\idle.png')
 playerIdle = pygame.transform.scale(playerIdle, (64, 64))
@@ -80,6 +105,7 @@ class Tile:
     def __init__(self, sprite, x, y):
         self.sprite = sprite
         self.x = x
+        self.y = y
 
 def checkCollision(rect1, rect2):
     if rect1.colliderect(rect2):
@@ -96,6 +122,17 @@ enemyDistanceCounter = 0
 enemyList = []
 backgroundList = [Tile(background, background.get_width() * i, 0) for i in range(backgroundMultiples)]
 
+foregroundList = []
+for j in range(len(foregroundTilemap)):
+    tileList = []
+    for i in range(len(foregroundTilemap[j])):
+        if foregroundTilemap[j][i] == 5:
+            tileList.append(Tile(foreground5, foreground5.get_width() * i, (screenHeight - groundRectHeight)))
+        elif foregroundTilemap[j][i] == 10:
+            tileList.append(Tile(foreground10, foreground10.get_width() * i, (screenHeight - groundRectHeight) + (j * 64)))
+
+    foregroundList.append(tileList)
+
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT or (event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE):
@@ -105,9 +142,12 @@ while True:
     keysPressed = pygame.key.get_pressed()
     
     screen.fill((0, 0, 0))
-    pygame.draw.rect(screen, (0, 255, 0), (0, screenHeight - groundRectHeight, screenWidth, groundRectHeight))
     for item in backgroundList:
-        screen.blit(item.sprite, (item.x, 0))
+        screen.blit(item.sprite, (item.x, item.y))
+    
+    for l in foregroundList:
+        for tile in l:
+            screen.blit(tile.sprite, (tile.x, tile.y))
 
     if gameHasRun == False and gameRunning == False:
         screen.blit(playerIdle, (player.x, player.y))
@@ -128,10 +168,19 @@ while True:
             backgroundList = backgroundList[1:]
         elif backgroundList[-1].x < screenWidth - background.get_width():
             backgroundList.append(Tile(background, backgroundList[-1].x + background.get_width(), 0))
-            
+        
         for item in backgroundList:
             item.x -= 1
-        
+
+        for j in range(len(foregroundList)):
+            if foregroundList[j][0].x < -foregroundList[j][0].sprite.get_width():
+                foregroundList[j] = foregroundList[j][1:]
+            elif foregroundList[j][-1].x < screenWidth:
+                foregroundList[j].append(Tile(foregroundList[j][-1].sprite, foregroundList[j][-1].x + foregroundList[j][-1].sprite.get_width(), foregroundList[j][-1].y))
+
+            for i in foregroundList[j]:
+                i.x -= gameVelocity
+
         if player.jumping:
             screen.blit(playerJump, (player.x, player.y))
         else:
@@ -158,12 +207,12 @@ while True:
 
             if enemy.x > -enemy.width:
                 enemy.draw()
-                enemy.x -= enemyVelocity
+                enemy.x -= gameVelocity
             else:
                 enemyList = enemyList[1:]
         
         score += .1
-        print(str(int(score)))
+        #print(str(int(score)))
 
     pygame.display.update()
     clock.tick(60)
